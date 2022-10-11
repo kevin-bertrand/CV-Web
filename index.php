c299a347da02521daa3240ef7f75cf8cebaefb65
Kevin Bertrand <73759450+kevin-bertrand@users.noreply.github.com>
2022-10-11 15:31:22 +0200

    Updating index.php

--- index.php
+++ index.php
@@ -46,9 +46,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#projects-section">Projets</a>
                     </li>
-                    <!--<li class="nav-item">
-                        <a class="nav-link" href="#contact">Contact</a>
-                    </li>-->
                 </ul>
             </div>
         </div>
