eafaa7364da8affec06e4fcbf4507766f24013d2
Kevin Bertrand <73759450+kevin-bertrand@users.noreply.github.com>
2022-11-21 15:53:59 +0100

    Add sections to projects

--- pages/projects.php
+++ pages/projects.php
@@ -1,13 +1,3 @@
-<?php
-$curl = curl_init();
-
-curl_setopt($curl, CURLOPT_URL, "http://cv.desyntic.com:2570/project");
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-$output = curl_exec($curl);
-curl_close($curl);
-$projects = json_decode($output, true);
-?>
-
 <section id="projects-section">
     <div class="container">
         <div class="row">
@@ -17,31 +7,22 @@ $projects = json_decode($output, true);
             </div>
         </div>
 
-        <?php
-            foreach(array_reverse($projects) as $project) {
-                ?>
-                <div class="row justify-content-between project-view align-items-center">
-                    <div class="col-md-6 project_image">
-                        <img src=<?= "public/img/" . $project["mediaPath"] ?> onclick=<?= 'showModal("public/img/' . $project['mediaPath'] . '","' . str_replace(" ", "&ensp", htmlspecialchars($project['title'])) . '")' ?> />
-                    </div>
+        <div class="container text-center projects-category-btns">
+            <div class="col align-self-center">
+                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                    <input type="radio" class="btn-check project-category-btn" name="btnradio" id="btnradio1" checked onclick="changeList('all')">
+                    <label class="btn btn-outline-primary btn-lg" for="btnradio1">Tous</label>
 
-                    <div class="col-md-5">
-                        <h3><?= $project["title"] ?></h3>
-                        <h4><?= $project["company"] ?></h4>
-                        <p class="text-muted"><small><i class="bi bi-clock"></i> <?= date("F Y", $project["date"]) ?></small></p>
-                        <p><?= $project["description"] ?></p>
-                        <?php
-                        if (is_null($project["github"]) === false) {
-                        ?>
-                            <p><a href=<?= $project["github"] ?> target="_blank"><i class="bi bi-github"></i> Lien GitHub du projet</a></p>
-                        <?php
-                        }
-                        ?>
-                    </div>
+                    <input type="radio" class="btn-check project-category-btn" name="btnradio" id="btnradio2" onclick="changeList('iOS')">
+                    <label class="btn btn-outline-primary btn-lg" for="btnradio2">iOS</label>
+
+                    <input type="radio" class="btn-check project-category-btn" name="btnradio" id="btnradio3" onclick="changeList('automation')">
+                    <label class="btn btn-outline-primary btn-lg" for="btnradio3">Automatisation</label>
                 </div>
-                <?php
-            }
-        ?>
+            </div>
+        </div>
+
+        <div id="projects-list"></div>
 
         <div id="modalImage" class="modal">
             <span class="close">&times;</span>
@@ -77,6 +58,68 @@ $projects = json_decode($output, true);
         span.onclick = function() {
             navigationBar.style.display = "block";
             modal.style.display = "none";
+        }
+
+        var projectsList = document.getElementById("projects-list");
+        var projects = {};
+        const months = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
+
+        function downloadProjects() {
+            $.ajax({
+                url: 'http://cv.desyntic.com:2570/project',
+            })
+            .done(function (data) {
+                data.sort(function(a, b){
+                    return new Date(b.date) - new Date(a.date)
+                });
+                projects = data;
+                changeList("all");
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                alert(xhr.responseText);
+                alert(textStatus);
+                console.log(errorThrown);
+            });
+        }
+
+        window.onload = downloadProjects;
+
+        function escapeHtml(text) {
+            var map = {
+                '&': '&amp;',
+                '<': '&lt;',
+                '>': '&gt;',
+                '"': '&quot;',
+                "'": '&#039;'
+            };
+            
+            return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+        }
+
+        function changeList(selection) {
+            removeList();
+
+            projects.forEach(project => {
+                if (selection === "all" || selection === project["category"]) {
+                    let projectDate = new Date(project.date * 1000);
+                    projectsList.innerHTML += `<div class="row justify-content-between project-view align-items-center"> \n \
+                        <div class="col-md-6 project_image"> \n \
+                            <img src="public/img/` + project.mediaPath + `" onclick="showModal('public/img/` + project.mediaPath + `','` + escapeHtml(project.title) + `')"/> \n \
+                        </div> \n \
+                        <div class="col-md-5"> \n \
+                        <h3>` + project.title + `</h3> \n \
+                        <h4>` + project.company + `</h4> \n \
+                        <p class="text-muted"><small><i class="bi bi-clock"></i> ` + months[projectDate.getMonth()] + ` ` + projectDate.getFullYear() + `</small></p> \n \
+                        <p>` + project.description + `</p> \n \ `
+                        + (("github" in project) ? (` <p><a href='` + project.github + `' target="_blank"><i class="bi bi-github"></i> Lien GitHub du projet</a></p>\n \ `) : ``) + `
+                    </div>
+                    `
+                }
+            });
+        }
+
+        function removeList() {
+            projectsList.innerHTML = '';
         }
     </script>
 </section>
